e24a7d47644af57901e2cc997d34252ea6369b86
lukas2005 <lukas2005.38@gmail.com>
2018-05-08 17:32:14 +0200

    Intial commit

---
+++ src/main.js
@@ -0,0 +1,123 @@
+const electron = require("electron");
+const {ipcMain: ipc, app, BrowserWindow} = electron;
+const {autoUpdater} = require("electron-updater");
+
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+
+const pug = require("pug");
+
+const IPFSFactory = require("ipfsd-ctl");
+const ipfsd = IPFSFactory.create();
+let ipfsNode;
+
+const userDataPath = app.getPath("userData");
+let configFile = path.join(userDataPath, "config.json");
+
+let config = {
+};
+
+let win; // this will store the window object
+
+let ipnsHash = "QmWdX5haFxJh1iBNhiextPsc6ifbvZGxqSyrte9QHgVGVU";
+
+// creates the default window
+function createDefaultWindow(file, options) {
+	win = new BrowserWindow({width: 900, height: 680});
+	loadPug(win, file, options);
+	win.on("closed", gracefulQuit);
+	return win;
+}
+
+let ipfsUrl;
+var globalStatus = "Waiting for the IPFS node";
+
+ipfsd.spawn({disposable: false, repoPath: "~/.ipfs"}, (err, ipfsNodee) => {
+	ipfsNode = ipfsNodee;
+
+	updateStatus("IPFS Node spawned.");
+
+	ipfsNode.start((err, ipfs) => {
+		handleErr(err);
+		updateStatus("IPFS Node started.");
+
+		updateStatus("Resolving the IPNS hash.");
+		ipfs.name.resolve(ipnsHash, (err, ipfsHash) => {
+			ipfs.pin.add(ipfsHash);
+
+			let urlObj = ipfsNode.gatewayAddr.nodeAddress();
+			let gatewayUrl = "http://"+urlObj.address+":"+urlObj.port;
+			ipfsUrl = gatewayUrl+"/ipfs/"+ipfsHash;
+
+			
+			console.log(ipfsUrl);
+			updateStatus("Url obtained. Loading...");
+		});
+	});
+});
+
+app.on("ready", () => {
+	//autoUpdater.checkForUpdates();
+
+	createDefaultWindow("loader", { url: url, status: globalStatus });
+});
+
+// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
+autoUpdater.on("update-downloaded", (info) => {
+	win.webContents.send("updateReady");
+});
+
+// when receiving a quitAndInstall signal, quit and install the new version ;)
+ipc.on("quitAndInstall", (event, arg) => {
+	autoUpdater.quitAndInstall();
+});
+
+ipc.on("app-loaded", (e, arg) => {
+	updateStatus("Done", true);
+});
+
+function loadPug(win, file, options) {
+	//let h = "data:text/html;charset=utf-8," + encodeURI(pug.renderFile(file));
+    
+	let pathToFile = path.join(__dirname, "public", file);
+
+	fs.writeFileSync(pathToFile+".html", pug.renderFile(pathToFile+".pug", options));
+
+	win.loadURL(url.format({
+		pathname: pathToFile+".html",
+		protocol: "file:",
+		slashes: true
+	}));
+}
+
+function gracefulQuit() {
+	ipfsNode.stop((err) => {
+		handleErr(err);
+		ipfsNode.cleanup((err) => {
+			handleErr(err);
+			if (fs.existsSync("~/.ipfs/repo.lock")) {
+				fs.unlinkSync("~/.ipfs/repo.lock");
+			}
+			app.quit();
+		});
+	});
+}
+
+function handleErr(err) {
+	if (err) {
+		updateStatus("Error: "+err);
+		setTimeout(gracefulQuit, 2000);
+		console.error(err);
+	}
+}
+function updateStatus(status, hideStatus) {
+	console.log(status);
+	globalStatus = status;
+	//ipc.emit("updateStatus", { url: ipfsUrl, status: status });
+	let data = { url: ipfsUrl, status: status };
+	if (hideStatus) data.hideStatus = hideStatus;
+	if (win) {
+		win.webContents.send("updateStatus", data);
+	}
+}
---
+++ src/public/src.js
@@ -0,0 +1,33 @@
+const electron = require("electron");
+const {ipcRenderer: ipc} = electron;
+
+let frame = document.getElementById("app");
+let spinner = document.getElementById("spinner");
+let statusText = document.getElementById("loading-text");
+
+
+let rects = document.getElementsByClassName("sk-rect");
+
+frame.style.visibility = "hidden";
+frame.onload = () => {
+	spinner.style.visibility = "hidden";
+	frame.style.visibility = "visible";
+	ipc.emit("app-loaded");
+};
+
+let i = 0;
+while(i < rects.length-1) {
+	let rect = rects.item(i);
+	rect.style["background-color"] = "white";
+	rect.style.width = "20px";
+
+	i++;
+}
+
+ipc.on("updateStatus", (e, args) => {
+	statusText.innerText = args.status;
+	frame.src = args.url;
+	if (args.hideStatus) {
+		statusText.parentElement.removeChild(statusText);
+	}
+});
